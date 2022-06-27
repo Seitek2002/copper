63e806e81d9045beee45689027beebcb8bfbee72
saliia <cygnuswinter@gmail.com>
2022-06-27 18:39:46 +0600

    add changes

--- js/reviews/reviews.js
+++ js/reviews/reviews.js
@@ -1,19 +1,30 @@
 new Swiper('.reviews__swiper', {
 
-    loop: true,
-  
-    pagination: {
-      el: '.reviews__swiper-swiper-pagination',
-      clickable: true,
-    },
-  
-    navigation: {
-      nextEl: '.reviews__swiper-button-next',
-      prevEl: '.reviews__swiper-button-prev',
+  loop: true,
+
+  pagination: {
+    el: '.reviews__swiper-swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.reviews__swiper-button-next',
+    prevEl: '.reviews__swiper-button-prev',
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
     },
 
-    slidesPerView: 3,
-    spaceBetween: 30,
-    centeredSlides: true,
+    480: {
+      slidesPerView: 2,
+    },
 
-  });
+    992: {
+      slidesPerView: 3,
+    },
+  }
+
+
+});
